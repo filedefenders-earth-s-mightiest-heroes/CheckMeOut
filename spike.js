9e1bbf5aded933fc103bc8cbdce55dfaacb4772d
jjblues86 <jeromejoof@yahoo.com>
2019-04-10 21:18:52 -0700

    we are getting data from the api

--- spike.js
+++ spike.js
@@ -9,20 +9,22 @@ const http = require("https");
 
 superagent.post('https://api.metadefender.com/v4/file')
     .set('apikey', `${process.env.API_KEY}`)
-    .set('content-type', 'application/octet-stream')
+    .set('Content-Type', 'application/octet-stream')
+    .send("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"\"; filename=\"C:\\Users\\JeromeJoof\\Desktop\\kayjay.jpg\"\r\nContent-Type: application/pdf\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--")
     // .set('filename', '@/path/to/https://www.faa.gov/regulations_policies/handbooks_manuals/aviation/phak/media/14_phak_ch12.pdf')
     .then(res => {
-        let req = http.request(superagent, function (res) {
-            let chunks = [];
-
-            res.on("data", function () {
-                let body = Buffer.concat(chunks);
-                console.log(body.toString());
-
-            });
-        });
-
-        req.write()
+        // console.log(res)
+        // let req = http.request(superagent, function (res) {
+        //     let chunks = [];
+        //
+        //     res.on("data", function () {
+        //         let body = Buffer.concat(chunks);
+        //         console.log(body.toString());
+        //
+        //     });
+        // });
+        //
+        // req.write("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"\"; filename=\"C:\\Users\\JeromeJoof\\Desktop\\kayjay.jpg\"\r\nContent-Type: application/pdf\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--")
 
     })
      .catch(console.log);
