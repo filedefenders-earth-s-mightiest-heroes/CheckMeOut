ae1e0ddbe42d6eabc5adbbca90d410113f433c5b
jjblues86 <jeromejoof@yahoo.com>
2019-04-10 19:50:03 -0700

    getting data from api

--- spike.js
+++ spike.js
@@ -11,11 +11,21 @@ superagent.post('https://api.metadefender.com/v4/file')
     .set('apikey', `${process.env.API_KEY}`)
     .set('content-type', 'application/octet-stream')
     // .set('filename', '@/path/to/https://www.faa.gov/regulations_policies/handbooks_manuals/aviation/phak/media/14_phak_ch12.pdf')
-    .then(console.log)
-    .catch(console.log);
+    .then(res => {
+        let req = http.request(superagent, function (res) {
+            let chunks = [];
 
-let req = http.request(options, function (res) {
-    let chunks = []
-})
+            res.on("data", function () {
+                let body = Buffer.concat(chunks);
+                console.log(body.toString());
+
+            });
+        });
+
+        req.write()
+
+    })
+     .catch(console.log);
+
 
 
