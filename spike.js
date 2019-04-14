a0a256ad284a3731057fac49992527f2e3306610
jjblues86 <jeromejoof@yahoo.com>
2019-04-13 17:53:12 -0700

    mvp

--- spike.js
+++ spike.js
@@ -11,20 +11,8 @@ superagent.post('https://api.metadefender.com/v4/file')
     .set('apikey', `${process.env.API_KEY}`)
     .set('Content-Type', 'application/octet-stream')
     .send("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"\"; filename=\"C:\\Users\\JeromeJoof\\Desktop\\kayjay.jpg\"\r\nContent-Type: application/pdf\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--")
-    // .set('filename', '@/path/to/https://www.faa.gov/regulations_policies/handbooks_manuals/aviation/phak/media/14_phak_ch12.pdf')
+
     .then(res => {
-        // console.log(res)
-        // let req = http.request(superagent, function (res) {
-        //     let chunks = [];
-        //
-        //     res.on("data", function () {
-        //         let body = Buffer.concat(chunks);
-        //         console.log(body.toString());
-        //
-        //     });
-        // });
-        //
-        // req.write("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"\"; filename=\"C:\\Users\\JeromeJoof\\Desktop\\kayjay.jpg\"\r\nContent-Type: application/pdf\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--")
 
     })
      .catch(console.log);
