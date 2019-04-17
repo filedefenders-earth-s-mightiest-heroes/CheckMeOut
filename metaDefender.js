cd6735d234829ba16eea5167f6874b6bab260659
jjblues86 <jeromejoof@yahoo.com>
2019-04-16 20:30:43 -0700

    having weird error

--- metaDefender.js
+++ metaDefender.js
@@ -1,33 +1,31 @@
 'use strict';
 
 const superagent = require('superagent');
+const morgan = require('morgan');
 
-const authorize = (req) => {
 
-    let code = req.query.code;
-    console.log('(1) CODE:', code);
 
-    return superagent.post('https://api.metadefender.com/v4/file')
-        .type('form')
-        .send({
-            code: code,
-            redirect_uri:`${process.env.API_KEY}`
-        })
 
-    // superagent.post('https://api.metadefender.com/v4/file').then(console.log).catch(console.error);
 
-        .end((err, res) => {
-       })
 
-    // // promise with async/await
-    // (async () => {
-    //     try {
-    //         const res = await superagent.post('https://api.metadefender.com/v4/file')
-    //         console.log(res);
-    //     } catch (err) {
-    //         console.error(err);
-    //     }
-    //
-    // })();
 
-}
+
+
+
+// const authorize = (req) => {
+//
+//     let code = req.query.code;
+//     console.log('(1) CODE:', code);
+//
+//     return superagent.post('https://api.metadefender.com/v4/file')
+//         .type('form')
+//         .send({
+//             code: code,
+//             redirect_uri:`${process.env.API_KEY}`
+//         })
+//
+//         .end((err, res) => {
+//        })
+//
+//
+// }
